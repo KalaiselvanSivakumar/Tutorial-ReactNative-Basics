62465f8f7e8f3dfe654d1877ea95d8392b0ab258
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2021-09-25 23:23:00 +0530

    Initial Hello World app

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center"
+			}}>
+			<Text>Hello, world!</Text>
+		</View>
+	);
+}
+
+export default HelloWorldApp;
