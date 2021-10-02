ae82b66e78abceae27de935104b1dfd2ea6df3cd
Kalai selvan Sivakumar <kalaiselvansivakumar@gmail.com>
2021-10-02 14:02:20 +0530

    Basics: React fundamentals

--- App.js
+++ App.js
@@ -3,96 +3,103 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
-import {
-	Button,
-	Image,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TextInput,
-	View,
-} from 'react-native';
-
-const HelloWorldApp = () => {
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-			<Text>Hello, world!</Text>
-		</View>
-	);
-};
-
-const styles = StyleSheet.create({
-	center: {
-		alignItems: 'center',
-	},
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-});
-
-const Greeting = props => {
-	return (
-		<View style={styles.center}>
-			<Text>Hello {props.name}!</Text>
-		</View>
-	);
-};
-
-const LotsOfGreetings = () => {
-	return (
-		<View style={[styles.center, { top: 50 }]}>
-			<Greeting name="Rexxar" />
-			<Greeting name="Jaina" />
-			<Greeting name="Valeera" />
-		</View>
-	);
-};
-
-const App = () => {
-	const [count, setCount] = useState(0);
-
-	return (
-		<View style={styles.container}>
-			<Text>You clicked {count} times.</Text>
-			<Button onPress={() => setCount(count + 1)} title="Click me!" />
-		</View>
-	);
-};
-
-const SimpleNativeComponents = () => {
-	return (
-		<ScrollView>
-			<Text>Some text</Text>
-			<View>
-				<Text>Some more text</Text>
-				<Image
-					source={{
-						uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-					}}
-					style={{width: 200, height: 200}}
-				/>
-			</View>
-			<TextInput
-				style={{
-					height: 40,
-					borderColor: 'gray',
-					borderWidth: 1
-				}}
-				defaultValue="Enter some text here"
-			/>
-		</ScrollView>
-	);
-}
+// import React, {useState} from 'react';
+// import {
+//   Button,
+//   Image,
+//   ScrollView,
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   View,
+// } from 'react-native';
+//
+// const HelloWorldApp = () => {
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//       <Text>Hello, world!</Text>
+//     </View>
+//   );
+// };
+//
+// const styles = StyleSheet.create({
+//   center: {
+//     alignItems: 'center',
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
+//
+// const Greeting = props => {
+//   return (
+//     <View style={styles.center}>
+//       <Text>Hello {props.name}!</Text>
+//     </View>
+//   );
+// };
+//
+// const LotsOfGreetings = () => {
+//   return (
+//     <View style={[styles.center, {top: 50}]}>
+//       <Greeting name="Rexxar" />
+//       <Greeting name="Jaina" />
+//       <Greeting name="Valeera" />
+//     </View>
+//   );
+// };
+//
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//
+//   return (
+//     <View style={styles.container}>
+//       <Text>You clicked {count} times.</Text>
+//       <Button onPress={() => setCount(count + 1)} title="Click me!" />
+//     </View>
+//   );
+// };
+//
+// const SimpleNativeComponents = () => {
+//   return (
+//     <ScrollView>
+//       <Text>Some text</Text>
+//       <View>
+//         <Text>Some more text</Text>
+//         <Image
+//           source={{
+//             uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+//           }}
+//           style={{width: 200, height: 200}}
+//         />
+//       </View>
+//       <TextInput
+//         style={{
+//           height: 40,
+//           borderColor: 'gray',
+//           borderWidth: 1,
+//         }}
+//         defaultValue="Enter some text here"
+//       />
+//     </ScrollView>
+//   );
+// };
 
 // export default HelloWorldApp;
 // export default LotsOfGreetings;
 // export default App;
-export default SimpleNativeComponents;
+// export default SimpleNativeComponents;
+
+/**
+ * Basics of React Native code block
+ */
+import Cafe from './basics/ReactFundamentals';
+
+export default Cafe;
---
+++ basics/ReactFundamentals.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {Button, Text, View} from 'react-native';
+
+const Cat = props => {
+  const [isHungry, setIsHungry] = useState(true);
+
+  return (
+    <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}
+      </Text>
+      <Button
+        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+      />
+    </View>
+  );
+};
+
+const Cafe = () => {
+  return (
+    <>
+      <Cat name="Munkustrap" />
+      <Cat name="Spot" />
+    </>
+  );
+};
+
+export default Cafe;
